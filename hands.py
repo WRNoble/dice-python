16cfde5195a8a6cadc900eb88d6f2d0dd9a2c06e
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-05 11:19:04 -0400

    added yatzyhand class

--- hands.py
+++ hands.py
@@ -1,3 +1,5 @@
+from dice import D6
+
 class Hand(list):
     def __init__(self, size=0, die_class=None, *args, **kwargs):
         if not die_class:
@@ -6,3 +8,8 @@ class Hand(list):
 
         for _ in range(size):
             self.append(die_class())
+        self.sort()
+
+class YatzyHand(Hand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(size=5, die_class=D6, *args, **kwargs)
