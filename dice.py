d37121b52fbbc1b6ca203ea8ab249b6514de5bd9
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-05 10:18:24 -0400

    added first die class

---
+++ dice.py
@@ -0,0 +1,9 @@
+import random
+
+class Die:
+    def __init__(self, sides=2, value=0):
+        if not sides >= 2:
+            raise ValueError("Must have at least 2 sides")
+        if not isinstance(sides, int):
+            raise ValueError("Sides Must be a whole number")
+        
