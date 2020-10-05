1a5fd7f58512b13dc8ea356f384690513ac7e8c1
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-05 11:27:09 -0400

    method that will allows us to get all of the same type of numbers

--- hands.py
+++ hands.py
@@ -10,6 +10,12 @@ class Hand(list):
             self.append(die_class())
         self.sort()
 
+    def _by_value(self, value):
+        dice = []
+        for die in self:
+            if die == value:
+                dice.append(die)
+        return dice
 class YatzyHand(Hand):
     def __init__(self, *args, **kwargs):
         super().__init__(size=5, die_class=D6, *args, **kwargs)
