8ce871a359505091a2cea8d6afb6f844a1566fe1
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-05 11:08:09 -0400

    included error message in hand class

--- hands.py
+++ hands.py
@@ -1,3 +1,5 @@
 class Hand(list):
-    def __init__(self, size=0, *args, **kwargs):
+    def __init__(self, size=0, die_class=None, *args, **kwargs):
+        if not die_class:
+            raise ValueError("You must provide a die class.")
         super().__init__()
